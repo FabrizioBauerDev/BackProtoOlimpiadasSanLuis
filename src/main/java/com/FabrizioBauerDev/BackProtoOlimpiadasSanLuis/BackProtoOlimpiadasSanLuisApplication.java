3b6ca32092ee981e10ea4b9b1cc31c43e68ddaf0
Fabrizio Riera Bauer <fabriziojriera@gmail.com>
2024-06-22 16:19:59 -0300

    First commit

---
+++ src/main/java/com/FabrizioBauerDev/BackProtoOlimpiadasSanLuis/BackProtoOlimpiadasSanLuisApplication.java
@@ -0,0 +1,13 @@
+package com.FabrizioBauerDev.BackProtoOlimpiadasSanLuis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackProtoOlimpiadasSanLuisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackProtoOlimpiadasSanLuisApplication.class, args);
+	}
+
+}
